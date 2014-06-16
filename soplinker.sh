94aba245261b28bdbcf283f843e84a7897934515
Simon Baev <gswcm@mini.gswcm.net>
2014-06-16 18:52:20 -0400

    Unfinished attempt to implement auto selection of working Sopcast URL

--- soplinker.sh
+++ soplinker.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# local definitions
 rport=8908
 lport=3908
 #--
@@ -10,7 +7,7 @@ selectItem() {
 		echo -1
 	else
 		for((i=0;i<${#array[@]};i++)); do
-			echo $(printf "%2d." $((i+1))) ${array[$i]} >&2
+			echo "$(printf "%2d." $((i+1)))" ${array[$i]} >&2
 		done
 		while(true); do
 			read -p "--> " x >&2
@@ -42,27 +39,37 @@ isInstalled() {
 	return 0
 }
 # Sanity check
-isInstalled "html2text"  || exit 1
-isInstalled "sp-sc-auth" || exit 2
-isInstalled "xsltproc"   || exit 3
-#--
-while read -p "Enter URL of a webpage with Sopcast links (Ctrl+D to quit): " url
+#--------------
+if [ $# -lt 1 ]; then
+	echo "Usage: $(basename $0) <URL of a webpage with sopcast links>" 1>&2
+	exit 10
+fi
+url="$1"
+isInstalled "html2text"		|| exit 1
+isInstalled "sp-sc-auth"	|| exit 2
+isInstalled "xsltproc"		|| exit 3
+isInstalled "wget"			|| exit 4
+
+# Main loop
+#-----------
+while true
 do
-	#-- Check if nothin is entered
-	[ -z "$url" ] && continue;
 	#-- Retrieve webpage and save it in a temp file
-	wget ${url} -O- 2> /dev/null | xsltproc --html minimal.xsl - 2> /dev/null | html2text > $$
+	wget --no-cache ${url} -O- 2> /dev/null | xsltproc --html minimal.xsl - 2> /dev/null | html2text > $$
 	#-- Let user select a SOP link
-	printf "\nSelect Sopcast link to stream into VLC player (or '0' to quit):\n"
+	printf "\nSelect Sopcast link to stream into VLC player (Ctrl+C to quit):\n"
 	if [ $(cat $$ | wc -l) -gt 0 ]; then
 		temp=($(cat $$))
+		printf "%2d. Auto selection (try all one by one to find good one).\n" 0
 		result=$(selectItem temp[@])
-		[ $result -eq 0 ] && break
-		echo "Starting Sopcast broadcaster..."
-		echo "This process may fail if link is no longer valid (e.g. banned)."
-		echo "In case of success (you continue seeing this message) you need to start VLC (or any other) player"
-		echo "and open http://<ip of this host>:$rport/tv.asf network stream. To quit broadcasting you need to hit Ctrl+C to start over or quit."
-		sp-sc-auth $(echo ${temp[$((result-1))]}) $lport $rport > /dev/null || { echo "" && continue; }
+		if [ $result -eq 0 ]; then 
+		else
+			echo "Starting Sopcast broadcaster..."
+			echo "This process may fail if link is no longer valid (e.g. banned)."
+			echo "In case of success (you continue seeing this message) you need to start VLC (or any other) player"
+			echo "and open http://<ip of this host>:$rport/tv.asf network stream. To quit broadcasting you need to hit Ctrl+C to start over or quit."
+			echo "sp-sc-auth $(echo ${temp[$((result-1))]}) $lport $rport > /dev/null" || { echo "" && continue; }
+		fi
 		unset temp	
 	fi
 done
